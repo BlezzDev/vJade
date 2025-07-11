523cd7c997e45c6b3d0028ffdfdf262c29804e06
Alfaro <jesusdavidalfa17@gmail.com>
2025-07-11 00:17:23 -0500

    commit 1

--- src/com/blezzdev/vjade/util/color/VJadeColor.java
+++ src/com/blezzdev/vjade/util/color/VJadeColor.java
@@ -2,6 +2,7 @@ package com.blezzdev.vjade.util.color;
 
 public class VJadeColor {
     public static class HSV {
+        public static VJadeColorHSV TRANSPARENT = new VJadeColorHSV(0.0f, 0.0f, 0.0f, 0.0f);
         public static VJadeColorHSV BLACK = new VJadeColorHSV(0.0f, 0.0f, 0.0f);
         public static VJadeColorHSV WHITE = new VJadeColorHSV(1.0f, 1.0f, 1.0f);
         public static VJadeColorHSV RED = new VJadeColorHSV(1.0f, 0.0f, 0.0f);
