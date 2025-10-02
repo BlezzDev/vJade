e4e1898a1305437344c49dc2b76533527fe644c4
Alfaro <jesusdavidalfa17@gmail.com>
2025-10-02 17:33:41 -0500

    Manage delayed events.

---
+++ src/main/java/com/blezzdev/vjade/core/manager/TimerManager.java
@@ -0,0 +1,29 @@
+package com.blezzdev.vjade.core.manager;
+
+import com.blezzdev.vjade.tools.time.TimerUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TimerManager {
+    private final List<TimerUnit> timerList = new ArrayList<>();
+
+    public void timerProcesses(double deltaTime) {
+        for (TimerUnit time : timerList) {
+            time.step((float) deltaTime);
+
+            if (time.getCurrentTime() >= time.getTimeWait()) {
+                time.exec();
+                timerList.remove(time);
+            }
+        }
+    }
+
+    public void clear() {
+        timerList.clear();
+    }
+
+    public void delay(float seconds, Runnable function) {
+        timerList.add(new TimerUnit(seconds, function));
+    }
+}
