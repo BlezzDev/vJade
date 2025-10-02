b95240fff8eea5c0585b587eb0a6a74c25d2e2ac
Alfaro <jesusdavidalfa17@gmail.com>
2025-10-02 18:09:08 -0500

    I only replaced it so that you could get the engine components more quickly in any VJObject, not just in scenes (for example, if you want to make a box in a different class than Scene and you need some Engine component)

--- src/main/java/com/blezzdev/vjade/objects/build/Screen.java
+++ src/main/java/com/blezzdev/vjade/objects/build/Screen.java
@@ -1,17 +1,3 @@
 package com.blezzdev.vjade.objects.build;
 
-import com.blezzdev.vjade.core.engine.Logger;
-import com.blezzdev.vjade.core.input.InputManager;
-import com.blezzdev.vjade.core.manager.TimerManager;
-import com.blezzdev.vjade.core.window.Monitor;
-import com.blezzdev.vjade.core.window.WindowLogic;
-import com.blezzdev.vjade.tools.VJade;
-
-public class Screen extends VJObject {
-    public Logger getLogger() { return VJade.getContext().getLogger(); }
-    public InputManager getInput() { return VJade.getContext().getInput(); }
-    public Monitor getMonitor() { return VJade.getContext().getMonitor(); }
-    public WindowLogic getLogic() { return VJade.getContext().getLogic(); }
-
-    public TimerManager getTimer() { return VJade.getContext().getLogic().getTimerManager(); }
-}
+public class Screen extends VJObject {}
--- src/main/java/com/blezzdev/vjade/objects/build/VJObject.java
+++ src/main/java/com/blezzdev/vjade/objects/build/VJObject.java
@@ -1,6 +1,12 @@
 package com.blezzdev.vjade.objects.build;
 
+import com.blezzdev.vjade.core.engine.Logger;
+import com.blezzdev.vjade.core.input.InputManager;
+import com.blezzdev.vjade.core.manager.TimerManager;
+import com.blezzdev.vjade.core.window.Monitor;
+import com.blezzdev.vjade.core.window.WindowLogic;
 import com.blezzdev.vjade.objects.processes.LogicProcesses;
+import com.blezzdev.vjade.tools.VJade;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,4 +45,11 @@ public class VJObject implements LogicProcesses {
             object.finish();
         }
     }
+
+    public Logger getLogger() { return VJade.getContext().getLogger(); }
+    public InputManager getInput() { return VJade.getContext().getInput(); }
+    public Monitor getMonitor() { return VJade.getContext().getMonitor(); }
+    public WindowLogic getLogic() { return VJade.getContext().getLogic(); }
+
+    public TimerManager getTimer() { return VJade.getContext().getLogic().getTimerManager(); }
 }
