5894cfc6d9ff2a258fd303cbebc4513ce06f9826
Alfaro <jesusdavidalfa17@gmail.com>
2025-10-04 11:14:59 -0500

    Finally fill this class with something.

--- src/main/java/com/blezzdev/vjade/objects/build/Screen.java
+++ src/main/java/com/blezzdev/vjade/objects/build/Screen.java
@@ -1,3 +1,13 @@
 package com.blezzdev.vjade.objects.build;
 
-public class Screen extends VJObject {}
+public class Screen extends VJObject {
+    private String identifier;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+}
