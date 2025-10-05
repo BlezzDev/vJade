198d64b6a1e9dcdc0a8374f1f62ad867140d2eb9
Alfaro <jesusdavidalfa17@gmail.com>
2025-10-05 15:19:44 -0500

    Forget about putting that there.

--- src/main/java/com/blezzdev/vjade/core/manager/ScreenManager.java
+++ src/main/java/com/blezzdev/vjade/core/manager/ScreenManager.java
@@ -69,6 +69,7 @@ public class ScreenManager {
     }
 
     public void setMainScreen(String currentScreen) {
+        this.lastScreen = currentScreen;
         setCurrentScreen(currentScreen);
     }
 
