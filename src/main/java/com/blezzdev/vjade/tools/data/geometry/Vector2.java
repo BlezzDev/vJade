019b26dea0090c63f5711bbbb574877b75752340
Alfaro <jesusdavidalfa17@gmail.com>
2025-10-09 20:24:04 -0500

    docs(data): Adds a quick start guide for Vector2

--- src/main/java/com/blezzdev/vjade/tools/data/geometry/Vector2.java
+++ src/main/java/com/blezzdev/vjade/tools/data/geometry/Vector2.java
@@ -1,5 +1,19 @@
 package com.blezzdev.vjade.tools.data.geometry;
 
+/// Vector2 is a mathematical class designed to
+/// manage coordinates in two-dimensional planes.
+///
+/// has 5 methods to modify its values in a more comfortable way:
+///
+/// - add() Acts as addition.
+/// - subtract() Acts as subtraction.
+/// - multiply() Acts as a multiplier.
+/// - divide() Acts as divisor.
+/// - modulate() Acts as modulator.
+///
+/// If you need to set values in the following way
+/// more directly you can use the corresponding setters (setX() and setY()).
+
 public class Vector2 {
     public float x;
     public float y;
