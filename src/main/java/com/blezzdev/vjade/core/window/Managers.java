6eb4c99f0c66568a3a64e6a407b2eb0fd2b2f474
Alfaro <jesusdavidalfa17@gmail.com>
2025-11-10 17:43:07 -0500

    docs(window): Adds detailed documentation in Managers class

--- src/main/java/com/blezzdev/vjade/core/window/Managers.java
+++ src/main/java/com/blezzdev/vjade/core/window/Managers.java
@@ -7,6 +7,23 @@ import com.blezzdev.vjade.core.manager.collider.CollisionManager;
 import com.blezzdev.vjade.core.manager.input.InputManager;
 import com.blezzdev.vjade.core.manager.timer.TimerManager;
 
+/** The {@code Managers} class is a <b>control class</b> dedicated
+ * to managing different managers responsible for specific tasks.
+ * <p>Here is a list of the current managers.</p>
+ * <ul>
+ *     <li><b>User interface manager</b> manages the internal tasks of the UIs.</li>
+ *     <li><b>Collision manager</b> manages collision internal tasks.</li>
+ *     <li><b>Screen manager</b> manages the life-cycle screens.</li>
+ *     <li><b>Canvas manager</b> manages texture batching.</li>
+ *     <li><b>Timer manager</b> manages delay actions.</li>
+ *     <li><b>Input manager</b> manages inputs of the keyboard, mouse and external controllers.</li>
+ * </ul>
+ *
+ * @author Alfaro
+ * @version 1
+ * @since vJade 1
+ * */
+
 public class Managers {
     private final UserInterfaceManager userInterface = new UserInterfaceManager();
     private final CollisionManager collision = new CollisionManager();
@@ -37,22 +54,17 @@ public class Managers {
     public InputManager getInput() {
         return input;
     }
-
     public CollisionManager getCollision() {
         return collision;
     }
-
     public UserInterfaceManager getUserInterface() {
         return userInterface;
     }
-
     public ScreenManager getScreen() {
         return screen;
     }
-
     public TimerManager getTimer() {
         return timer;
     }
-
     public CanvasManager getCanvas() { return canvas; }
 }
