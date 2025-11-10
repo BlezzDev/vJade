97c558285627f28a87c555a1ce116cc6d442f9d7
Alfaro <jesusdavidalfa17@gmail.com>
2025-11-10 17:42:57 -0500

    docs(window): Adds detailed documentation in Monitor

--- src/main/java/com/blezzdev/vjade/core/window/Monitor.java
+++ src/main/java/com/blezzdev/vjade/core/window/Monitor.java
@@ -6,6 +6,24 @@ import org.lwjgl.glfw.GLFWVidMode;
 import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
 import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
 
+/**
+ * The {@code Monitor} class is responsible for <b>retrieving and storing information</b>
+ * about the display monitor where the application window is hosted.
+ *
+ * <p>The following parameters can be accessed:</p>
+ *
+ * <ul>
+ *   <li><b>Resolution</b> obtains the screen resolution.</li>
+ *   <li><b>Refresh rate</b> retrieves the monitor’s refresh rate (in Hertz).</li>
+ *   <li><b>Red, Green,</b> and <b>Blue bits</b> report the number of bits used for each color channel.</li>
+ * </ul>
+ *
+ * @author Alfaro
+ * @version 1.0
+ * @since vJade 1.0
+ */
+
+
 public class Monitor {
     private final GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
 
